e4853f0c2a3843560eacf855a1eeb9fff68982c9
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-04-12 16:01:03 -0500

    Created click button and array

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 $(document).ready(nowReady);
 
+let employeeInfo =[];
+
 function nowReady() {
     console.log('JQ');
-    
+    $('.submitButton').on('click', submitInfo)
+}
+
+function submitInfo() {
+    let newEmployerInfo ={
+        firstName: $('.firstNameInput').val(),
+        lastName: $('.lastNameInput').val(),
+        employeriD: $('.idInput').val(),
+        title: $('.titleInput').val(),
+        annualSalary: $('.salaryInput').val()
+}
+    employeeInfo.push(newEmployerInfo);
+    console.log('Clicked Submit Button', employeeInfo); 
+
 }
