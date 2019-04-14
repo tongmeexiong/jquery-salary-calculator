136b816a0017728ade206d4d11a48cceb793f11a
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-04-14 13:32:58 -0500

    Added Dom manipulation function

--- script.js
+++ script.js
@@ -4,18 +4,30 @@ let employeeInfo =[];
 
 function nowReady() {
     console.log('JQ');
-    $('.submitButton').on('click', submitInfo)
+    $('.submitButton').on('click', submitInfo);
+    displayInfo();
 }
 
 function submitInfo() {
-    let newEmployerInfo ={
+    let employeeObject ={
         firstName: $('.firstNameInput').val(),
         lastName: $('.lastNameInput').val(),
         employeriD: $('.idInput').val(),
         title: $('.titleInput').val(),
         annualSalary: $('.salaryInput').val()
 }
-    employeeInfo.push(newEmployerInfo);
+    employeeInfo.push(employeeObject);
     console.log('Clicked Submit Button', employeeInfo); 
+    displayInfo()
+}
 
+function displayInfo() {
+    console.log('DisplayInfo');
+    let employeeInput = $('#salaryCalculated');
+    employeeInput.empty();
+    for (let i = 0; i < employeeInfo.length; i++ ){
+        const calculatedList = `<th>${employeeInfo[i].firstName} ${employeeInfo[i].lastName} 
+        ${employeeInfo[i].employeriD} ${employeeInfo[i].title} ${employeeInfo[i].annualSalary}</th>`;
+        employeeInput.append(calculatedList);
+    }
 }
