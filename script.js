656326b64410861946e57de05bfc6ad8a53eb277
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-04-15 08:41:15 -0500

    Added delete button

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 $(document).ready(nowReady);
 
-let employeeInfo =[];
-let monthlyCosts = [];
-let monthlyAdjustment = 12;
+let employeeInfo = [];
+let id = [];
 let sumOfSalary = 0;
+let monthlyAdjustment = 12;
 
 
 function nowReady() {
     console.log('JQ');
     $('.submitButton').on('click', submitInfo);
     displayInfo();
-    calculateMonthlyAdjustment() 
-    addAnnualSalary()
+    calculateMonthlyAdjustment();
+    $('.deleteButton').on('click', deleteButton);
 }
 
 function submitInfo() {
-    let employeeObject ={
+    let employeeObject = {
         firstName: $('.firstNameInput').val(),
         lastName: $('.lastNameInput').val(),
         employeriD: $('.idInput').val(),
         title: $('.titleInput').val(),
         annualSalary: $('.salaryInput').val()
-}
+    }
     employeeInfo.push(employeeObject);
-    monthlyCosts.push(employeeObject.annualSalary);
-    console.log('Clicked Submit Button', employeeInfo); 
+    sumOfSalary += parseFloat(employeeObject.annualSalary);
+    console.log('Clicked Submit Button', employeeInfo);
+    console.log(sumOfSalary);
     displayInfo()
 }
 
@@ -32,28 +33,39 @@ function displayInfo() {
     console.log('DisplayInfo');
     let employeeInput = $('#salaryCalculated');
     employeeInput.empty();
-    for (let i = 0; i < employeeInfo.length; i++ ){
+    for (let i = 0; i < employeeInfo.length; i++) {
         const calculatedList = `<ul>${employeeInfo[i].firstName} ${employeeInfo[i].lastName} 
-        ${employeeInfo[i].employeriD} ${employeeInfo[i].title} ${employeeInfo[i].annualSalary}</ul>`;
+        ${employeeInfo[i].employeriD} ${employeeInfo[i].title} ${employeeInfo[i].annualSalary} $</ul>`;
+
         employeeInput.append(calculatedList);
         calculateMonthlyAdjustment()
-        addAnnualSalary() 
     }
 }
 
-
-function addAnnualSalary() {
-    for (let i = 0; i < monthlyCosts.length; i++) {
-        sumOfSalary = sumOfSalary + monthlyCosts[i];
-    }
-}
 
 function calculateMonthlyAdjustment() {
     console.log('Calculation');
     let totalMonthlyCost = $('.annualSalaryCalculated');
-    totalMonthlyCost.empty ();
-    for (let i = 0; i < monthlyCosts.length; i++) {
-        let theMonthlyCost = sumOfSalary / monthlyAdjustment;
-        totalMonthlyCost.append(theMonthlyCost);
-    }  
-}
+    totalMonthlyCost.empty();
+        let theMonthlyCost = sumOfSalary / 12;
+        totalMonthlyCost.append(`<div>Total Monthly: ${theMonthlyCost} </div>`);
+    }
+
+
+    function deleteButton() {
+        console.log('Delete Button');
+        let employeeObject = {
+            firstName: $('.firstNameInput').val(),
+            lastName: $('.lastNameInput').val(),
+            employeriD: $('.idInput').val(),
+            title: $('.titleInput').val(),
+            annualSalary: $('.salaryInput').val()
+    }
+    id.push(employeeObject.employeriD)
+        for(let i = 0; i < employeeInfo.length; i++) {
+            if (employeeInfo[i].employeriD === employeeObject.employeriD);
+            employeeInfo.splice(i,1);
+            
+        }
+        submitInfo();
+    }
